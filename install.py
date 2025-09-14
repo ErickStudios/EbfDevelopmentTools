9481e32f7fa42e6b5a3d0cd71fc6c2d29e8ec895
ErickStudios <152753651+ErickStudios@users.noreply.github.com>
2025-09-14 12:19:48 -0600

    a
    b

--- install.py
+++ install.py
@@ -10,6 +10,9 @@ import os
 # requests para enviar solicitudes
 import requests
 
+# for system things
+import sys
+
 #
 # YesNo
 #
@@ -45,12 +48,63 @@ kelly_path = "Kelly"
 #
 BootloaderUrl = "https://github.com/ErickStudios/KellyBootloader/raw/refs/heads/main/image/efi/boot/bootx64.efi"
 
+def waitkey():
+    if os.name == 'nt':  # Windows
+        import msvcrt
+        return msvcrt.getch().decode('utf-8', errors='ignore')
+    else:  # Unix-like (Linux/macOS)
+        import tty
+        import termios
+
+        fd = sys.stdin.fileno()
+        old_settings = termios.tcgetattr(fd)
+        try:
+            tty.setraw(fd)
+            tecla = sys.stdin.read(1)
+        finally:
+            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+        return tecla
+
+def clearscreen():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+def MenuDefine(Title:str,Options: list[str]) -> str:
+	OptionSel = 0
+    
+	key = ""
+
+	while True:
+		print(f"\033[37m\033[40m{Title}")
+		clearscreen()
+		print(f"\033[37m\033[40m{Title}")
+		for index, value in enumerate(Options):
+			colour = ""
+			if index == OptionSel:
+				colour = "\033[30m\033[47m"
+				if key.lower() == "e":
+					return value
+			else:
+				colour = "\033[37m\033[40m"
+
+			print(f"{colour}{value}")
+
+		print("W/S=move E=Select")
+
+		key = waitkey()
+		print(key)
+          
+		if key.lower() == "w":
+			OptionSel = OptionSel - 1
+		if key.lower() == "s":
+			OptionSel = OptionSel + 1
+     
 #
 # CreateDirs
 #
 # crea los directorios
 # 
-def CreateDirs():
+def CreateDirs(
+):
     # crear carpetas
     os.makedirs(efi_boot_path, exist_ok=True)
     os.makedirs(kelly_path, exist_ok=True)
@@ -63,23 +117,30 @@ def CreateDirs():
 def DownloadFile(
         Url: str,
         File: str
-):
+) -> bool:
     
-    # intentar encontrar el contenido del .efi
-    response = requests.get(Url)
+	# intentar encontrar el contenido del .efi
+	response = requests.get(Url)
 
-    # si se pudo
-    if response.status_code == 200:
-        # abrir el archivo
-        with open(File, "wb") as f:
-            # escribirlo
-            f.write(response.content)
+	# si se pudo
+	if response.status_code == 200:
+		# abrir el archivo
+		if os.path.exists(File):
+			with open(File, "rb") as f:
+				if f.read() == response.content:
+					return False
 
-        # avisar de el exito
-        print(f"{Url} Descargado correctamente")
-    else:
-        # no se pudo
-        print(f"Error al descargar {Url}: {response.status_code}")
+		with open(File, "wb") as f:
+			# escribirlo
+			f.write(response.content)
+
+		# avisar de el exito
+		print(f"{Url} Descargado correctamente")
+		return True
+	else:
+		# no se pudo
+		print(f"Error al descargar {Url}: {response.status_code}")
+	return False
 
 #
 # DownloadEfi
@@ -87,21 +148,7 @@ def DownloadFile(
 # descarga el bootx64.efi
 #
 def DownloadEfi():
-    # intentar encontrar el contenido del .efi
-    response = requests.get(BootloaderUrl)
-
-    # si se pudo
-    if response.status_code == 200:
-        # abrir el archivo
-        with open(bootx64_path, "wb") as f:
-            # escribirlo
-            f.write(response.content)
-
-        # avisar de el exito
-        print(f"Descargado correctamente")
-    else:
-        # no se pudo
-        print(f"Error al descargar: {response.status_code}")
+    DownloadFile(BootloaderUrl, bootx64_path)
 
 #
 # DownloadDependences
@@ -127,6 +174,21 @@ CreateDirs()
 # descargar el .efi
 print("Descargando el ejecutable de kellybootloader...")
 DownloadEfi()
+
+# iniciar la tienda
+if False:
+	while True:
+		Option = MenuDefine("Bienvenido a la tienda de KellyBootloader puedes hacer lo siguiente",
+		{
+			"Descargar Apps",
+			"Actualizar KellyBootloader", 
+			"Salir"
+		})
+		
+		if Option == "Actualizar KellyBootloader":
+			DownloadEfi()
+		if Option == "Salir":
+			exit(0)
 
 # preguntar si se quiere descargar las dependencias
 print("Desea descargar las dependencias (S/Y)/N: ")
